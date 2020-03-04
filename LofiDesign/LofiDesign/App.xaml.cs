f2a9ad8bcfbadd102e66ec5e59bf3bbc90b4ca8f
Tiffanatic <madsrynord@live.dk>
2020-03-04 08:16:44 +0100

    Add project files.

---
+++ LofiDesign/LofiDesign/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LofiDesign
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //adding basic stack navigation with root page
+            var nav = new NavigationPage(new ChooseExercise());
+            //setting startup page
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LofiDesign/LofiDesign/ChooseExercise.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LofiDesign
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChooseExercise : ContentPage
+    {
+        public ChooseExercise()
+        {
+            InitializeComponent();
+            title.Text = "Choose Exercise from code";
+        }
+
+        public async void OnExerciseClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExercisePage());
+        }
+    }
+}
